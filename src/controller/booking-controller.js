67cef8be1c13ae907f378939d7735b5261816387
Rushabh1105 <rushabh.sakharkar1998@gmail.com>
2023-01-11 01:11:36 +0530

    added message queue

--- src/controller/booking-controller.js
+++ src/controller/booking-controller.js
@@ -1,29 +1,47 @@
 const { StatusCodes } = require("http-status-codes");
-
 const { BookingServiece } = require("../servieces/index");
+const { createChannel, publishMessage, } = require('../utils/message-queue')
+const { REMAINDER_BINDING_KEY } = require("../config/server-config")
 
 const bookingServiece = new BookingServiece;
 
-const createBooking = async (req, res) => {
-    try {
-        const response = await bookingServiece.createBooking(req.body);
-        //console.log(response);
-        return res.status(StatusCodes.OK).json({
-            message : "Successfully completed booking",
-            success : true,
-            data : response,
-            err : {},
-        });
-    } catch (error) {
-        return res.status( error.statusCodes).json({
-            message : error.message,
-            success : false,
-            data : {},
-            err : error.explanation,
-        });
+class BookingController{
+
+    constructor(){
     }
+
+    async sendMessageToQueue(req, res){
+        const channel = await createChannel();
+        const data = {message : 'success'}
+        publishMessage(channel, REMAINDER_BINDING_KEY, JSON.stringify(data));
+        return res.status(200).json({
+            message : 'successfully published the event',
+            
+        })
+    }
+
+
+    async createBooking(req, res){
+        try {
+            const response = await bookingServiece.createBooking(req.body);
+            //console.log(response);
+            return res.status(StatusCodes.OK).json({
+                message : "Successfully completed booking",
+                success : true,
+                data : response,
+                err : {},
+            });
+        } catch (error) {
+            return res.status( error.statusCodes).json({
+                message : error.message,
+                success : false,
+                data : {},
+                err : error.explanation,
+            });
+        }
+    
+    }
+
 }
 
-module.exports = {
-    createBooking,
-}
+module.exports = BookingController;
--- src/routes/v1Routes/v1Routes.js
+++ src/routes/v1Routes/v1Routes.js
@@ -2,14 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
+//const { createChannel } = require("../../utils/message-queue");
 const BookingController = require("../../controller/booking-controller");
 
-router.post("/booking", BookingController.createBooking);
+//const channel = await createChannel();
+const bookingController = new BookingController();
 
-
-
-
-
-
+router.post("/booking", bookingController.createBooking);
+router.post('/publish', bookingController.sendMessageToQueue);
 
 module.exports = router;
